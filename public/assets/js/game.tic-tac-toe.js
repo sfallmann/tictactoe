b00841a3d9e9d5ebc49f62b66a1e4425ccf0234b
Sean Fallmann <jorundr@hotmail.com>
2017-04-09 11:59:21 -0400

    Cleanup code and finish game

---
+++ public/assets/js/game.tic-tac-toe.js
@@ -0,0 +1,185 @@
+
+;
+(function(window, _){
+  'use strict';
+  if(typeof require !== 'undefined'){
+    var _ = require('lodash');
+  }
+
+  var GAMESTATES = {
+    DRAW: 'DRAW',
+    CONTINUE: 'CONTINUE'    
+  }
+
+  window.GAMESTATES = window.GAMESTATES || GAMESTATES;
+
+  function createBoard(cols, rows) {
+    var board = [], col;
+    for (var i = 0; i < rows; i++) {
+      col = [];
+      for (var j = 0; j < cols; j++) {
+        col.push('');
+      }
+      board.push(col);
+    }
+    return board;
+  }
+
+  function isBoardEmpty(board){
+    for (var i = 0; i < rows; i++) {
+      for (var j = 0; j < cols; j++) {
+        if (board[i][j]){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  function returnTile(x, y, token) {
+    return {x: x, y: y, token: token}
+  }
+
+  function setTile(board, x, y, token) {
+    board[x][y] = token;
+    return returnTile(x, y, token);
+  }
+
+  function humanTurn(tile){
+    return tile;
+  }
+
+  function computerTurn(state, computer, opponent){
+
+    if (state.counter === 0){
+      return {x: 0, y: 0}
+    }
+
+    var choice;
+
+    miniMax(state, 0);
+
+    return choice;
+    
+    function miniMax(state, depth) {
+      var result = checkForWin(state.board, state.counter);
+      if (result === computer){
+        return 10 - depth;
+      } else if (result === opponent) {
+        return depth - 10;
+      } else if (result === GAMESTATES.DRAW) {
+        return 0;
+      }
+
+      depth++;
+
+      var moves = [];
+      var scores = [];
+      var token = state.players[0].token;
+      var newState;
+
+      for (var x = 0; x < 3; x++) {
+        for (var y = 0; y < 3; y++){
+          if (!state.board[x][y]) {
+            newState = _.cloneDeep(state);
+            newState.board[x][y] = token;
+            newState.counter++;
+            changePlayers(newState.players);
+
+            scores.push(miniMax(newState, depth))
+            moves.push({x: x, y: y});
+          }
+        }
+      }
+      if (token === computer){
+        var maxValue = scores.reduce(function(acc, curr){
+          return Math.max(acc, curr);
+        });
+        var maxIndex = scores.indexOf(maxValue);
+        choice = moves[maxIndex];
+        return scores[maxIndex];
+      }
+
+      if (token === opponent){
+        var minValue = scores.reduce(function(acc, curr){
+          return Math.min(acc, curr);
+        });
+        var minIndex = scores.indexOf(minValue);
+        choice = moves[minIndex];
+        return scores[minIndex];
+      }  
+
+    }
+  }
+
+  function createState(game){
+    var state = {
+      board: game.board.slice(),
+      players: game.players.slice(),
+      counter: game.counter
+    }
+    return state;
+  }
+
+
+  function checkForWin(board, counter) {
+
+    for (var x = 0, y = 0; x < 3, y < 3; x++, y++) {
+      if ((board[x][0] === board[x][1] && board[x][1] === board[x][2]) && board[x][0]){
+        return board[x][0];
+      }
+      if ((board[0][y] === board[1][y] && board[1][y] === board[2][y]) && board[0][y]){
+        return board[0][y];
+      }   
+    }
+
+    if ((board[0][0] === board[1][1] && board[1][1] === board[2][2]) && board[0][0]){
+      return board[0][0];
+    } 
+
+    if ((board[0][2] === board[1][1] && board[1][1] === board[2][0]) && board[0][2]){
+      return board[0][2];
+    } 
+
+    if (counter === 9){
+      return GAMESTATES.DRAW;
+    }
+
+    return GAMESTATES.CONTINUE;
+  }
+
+  function changePlayers(players){
+    var player = players.pop();
+    players.unshift(player);
+  }
+
+  window.TicTacToe = window.TicTacToe || function TicTacToe(p1, p2) {
+
+    var counter = 0;
+    var board = createBoard(3, 3);
+
+    return {
+      counter: counter,
+      players: [p1, p2],
+      board: createBoard(3, 3),
+      turn: function turn(x, y){
+        var tile;
+        var token = this.players[0].token
+        var type = this.players[0].type;
+        if (type === 'computer'){
+          var opponent = this.players[1].token;
+          var result = computerTurn(createState(this), token, opponent);
+          x = result.x;
+          y = result.y;
+        }
+        tile = setTile(this.board, x, y, token)
+        this.counter++;
+        changePlayers(this.players)
+        return tile;
+      },
+      checkForWin: checkForWin
+    }
+
+  }
+})(window, _);
+
---
+++ public/assets/js/ui.tic-tac-toe.js
@@ -0,0 +1,166 @@
+;  
+(function(window) {
+  'use strict';  
+  window.ui = window.ui || {
+    components: {
+      gridObj: document.getElementById('grid-object'),
+      name: document.querySelector('[name=firstname]'),
+      token: function token(){
+        return document.querySelector('[name=token]:checked');       
+      },
+      startBtn: document.getElementById("start"),
+      playAgainBtn: document.getElementById("play-again"),
+      modal: document.getElementById("modal"),
+      modalMsg: document.getElementById("modal-msg"),
+      gameboard: document.getElementById('gameboard'),
+      gameoptions: document.getElementById('gameoptions'),
+    },
+    init: function init(player1, player2) {
+      this.game = TicTacToe(player1, player2);    
+      this.components.gamegrid = this.components.gridObj.contentDocument;      
+      this.components.gamegrid.addEventListener("click", this.clickedSquare.bind(this));
+      this.playTurn();
+    },
+    toggleGameScreens: function() {
+      this.components.gameoptions.classList.toggle('hidden');
+      this.components.gameboard.classList.toggle('hidden');            
+    },
+    emptyBoard: function emptyBoard() {
+      var tokens = this.components.gamegrid.querySelectorAll('[id*=O-]', '[id*=X-]');
+      [].slice.call(tokens).forEach(function(token) {
+        token.style.fillOpacity = 0;
+      });
+    },
+    displayMessage: function(msg) {
+      var messages = document.querySelector('#messages');
+      messages.innerHTML = msg;
+    },
+    placeToken: function placeToken(target, token) {
+
+      var values = (target.id.split('-'));
+      var tokenId = token + '-' + values[1] + '-' + values[2];
+
+      this.components.gamegrid.getElementById(tokenId).style.fillOpacity = 1;
+    },
+    clickedSquare: function clickedSquare(e) {
+      
+      var status = this.game.checkForWin(this.game.board, this.game.counter);
+
+      if (status === GAMESTATES.CONTINUE && this.game.players[0].type === 'human') {
+        
+        if (!e.target.classList.contains('square')) {
+          return;
+        }
+
+        var values = e.target.id.split('-')
+
+        if (this.game.board[values[1]][values[2]]){
+          return;
+        }
+
+        var token = this.game.players[0].token;
+
+        this.game.turn(values[1], values[2]);
+        this.placeToken(e.target, token);
+        this.turnComplete();
+
+      }
+    },
+    playTurn: function playTurn() {
+      this.displayMessage(this.game.players[0].name + '\'s turn');
+      if (this.game.players[0].type === 'computer') {
+        setTimeout(this.computersTurn.bind(this), 1500);
+      }
+    },
+    computersTurn: function computersTurn() {
+      var gridsvg = this.components.gamegrid.getElementById('grid-svg');
+      gridsvg.classList.toggle('clickable');
+
+      var token = this.game.players[0].token;
+      var tile = this.game.turn();  
+      var tokenId = token + '-' + tile.x + '-' + tile.y;
+
+      this.placeToken(this.components.gamegrid.getElementById(tokenId), token);
+      gridsvg.classList.toggle('clickable');
+
+      this.turnComplete();       
+    },
+    turnComplete: function turnComplete() {
+      var status = this.game.checkForWin(this.game.board, this.game.counter);
+
+      if (status !== GAMESTATES.CONTINUE){
+        this.components.gamegrid.removeEventListener("click", this.clickedSquare);
+        if(status !== GAMESTATES.DRAW){
+          var player = (this.game.players.filter(function(player){
+            return player.token == status;
+          }))[0];
+          this.displayMessage(player.name + ' has won the game!!!');
+        } else {
+          this.displayMessage(status);
+        }    
+        this.components.playAgainBtn.classList.toggle('hidden');
+        return;
+      }
+
+      this.playTurn();
+
+    },
+    playAgain: function() {
+      this.emptyBoard();
+      this.displayMessage('');
+      this.components.playAgainBtn.classList.toggle('hidden');
+      this.toggleGameScreens();
+    },
+    start: function start() {
+
+      if (this.components.token().value === null){
+        this.components.startBtn.disabled = true;
+        this.components.modalMsg.innerHTML = 'Please select a token!';
+        this.components.modal.classList.toggle('hidden');
+        return;
+      }
+
+      if (!this.components.name.value) {
+        this.components.startBtn.disabled = true;
+        this.components.modalMsg.innerHTML = 'Please enter your name!';
+        this.components.modal.classList.toggle('hidden');
+        return;
+      }
+
+      var player1, player2;
+      if (this.components.token().value === 'O'){
+        player1 = {
+          name: this.components.name.value,
+          token: 'O',
+          type: 'human'
+        };
+        player2 = {
+          name: 'Computer',
+          token: 'X',
+          type: 'computer'
+        };
+      } else {
+        player1 = {
+          name: 'Computer',
+          token: 'O',
+          type: 'computer'
+        };  
+        player2 = {
+          name: this.components.name.value,
+          token: 'X',
+          type: 'human'
+        };     
+      }
+
+      this.toggleGameScreens();
+
+      this.components.gridObj.onload = function(){
+        ui.init(player1, player2);  
+      } 
+    },
+    closeModal: function closeModal() {
+      this.components.startBtn.disabled = false;
+      this.components.modal.classList.toggle('hidden');
+    }  
+  };
+})(window);
